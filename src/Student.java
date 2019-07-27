143b670828b7ecf6d84be14241cead810dad81ac
lsandathara <dmlakchani@gmail.com>
2019-07-27 21:32:08 +0530

    student class changes with getter and setters

--- src/Student.java
+++ src/Student.java
@@ -1,7 +1,20 @@
 
 public class Student {
-	String studentid;
-	String name;
+	private String studentid;
+	private String name;
+	public String getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(String studentid) {
+		this.studentid = studentid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 	
 
